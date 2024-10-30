a5d0740b4f666b631f938101106a6239c74f9524
Marcel Zielinski <marziel2001@gmail.com>
2024-10-30 13:54:53 +0100

    Get all cats and sort pets

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -23,6 +24,26 @@ namespace Training.DomainClasses
             {
                 _petsInTheStore.Add(newPet);
             }
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var result = new List<Pet>(_petsInTheStore);
+
+            result.Sort((pet1, pet2) => pet1.name.CompareTo(pet2.name));
+
+            return result;
         }
     }
 }
