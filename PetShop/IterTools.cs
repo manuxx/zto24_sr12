e3f9edd155a8bb5676268053c5f168dab480cdd7
Mateusz <mateusz.naklicki06@gmail.com>
2024-10-30 13:00:41 +0100

    Rename pets to items

--- PetShop/IterTools.cs
+++ PetShop/IterTools.cs
@@ -4,11 +4,11 @@ namespace Training.DomainClasses
 {
     public static class IterTools
     {
-        public static IEnumerable<TItem> OneAtATime<TItem>(this IList<TItem> pets)
+        public static IEnumerable<TItem> OneAtATime<TItem>(this IList<TItem> items)
         {
-            foreach (var pet in pets)
+            foreach (var item in items)
             {
-                yield return pet;
+                yield return item;
             }
         }
     }
