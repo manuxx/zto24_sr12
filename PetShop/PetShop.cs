744277b4d64e4395eaf739de5f15329be06454c9
mikibak <107919074+mikibak@users.noreply.github.com>
2024-10-30 13:29:55 +0100

    Add read only set

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -14,7 +15,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore.OneAtATime();
+            return new ReadOnlySet<Pet>(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
---
+++ PetShop/ReadOnlySet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public class ReadOnlySet<Pet> : IEnumerable<Pet>
+    {
+        private readonly IList<Pet> _items;
+
+        public ReadOnlySet(IList<Pet> items)
+        {
+            _items = items;
+        }
+
+        public IEnumerator<Pet> GetEnumerator()
+        {
+            return _items.OneAtATime().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
