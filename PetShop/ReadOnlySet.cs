c488d1a6fddb62a7c9c2d12f6011a97aedfd9208
Mateusz <mateusz.naklicki06@gmail.com>
2024-10-30 13:28:23 +0100

    Use ReadOnlySet

--- PetShop/ReadOnlySet.cs
+++ PetShop/ReadOnlySet.cs
@@ -1,33 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
 {
-    public class PetShop
-    {
-        private IList<Pet> _petsInTheStore;
-
-        public PetShop(IList<Pet> petsInTheStore)
-        {
-            this._petsInTheStore = petsInTheStore;
-        }
-
-        public IEnumerable<Pet> AllPets()
-        {
-            //return _petsInTheStore.OneAtATime();
-            return new ReadOnlySet(_petsInTheStore);
-        }
-
-        public void Add(Pet newPet)
-        {
-            if (!_petsInTheStore.Contains(newPet))
-            {
-                _petsInTheStore.Add(newPet);
-            }
-        }
-    }
-
     public class ReadOnlySet : IEnumerable<Pet>
     {
         public IEnumerable<Pet> items { get; set; }
@@ -36,7 +11,7 @@ namespace Training.DomainClasses
         {
             this.items = items;
         }
-
+                                                                                           
         public IEnumerator<Pet> GetEnumerator()
         {
             foreach (var item in items)
