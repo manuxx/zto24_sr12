9a7bd198af9be6441c826c0d49371a68a6f7cdaa
Marcel Zielinski <marziel2001@gmail.com>
2024-11-06 12:59:17 +0100

    All green

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -7,7 +7,49 @@ namespace Training.DomainClasses
     {
         private IList<Pet> _petsInTheStore;
 
-        public PetShop(IList<Pet> petsInTheStore)
+
+        public IEnumerable<Pet> AllOfSpecie(Func<Pet, bool> comparer)
+        {
+	        foreach (var pet in _petsInTheStore)
+	        {
+		        if (comparer(pet)) yield return pet;
+	        }
+        }
+		public IEnumerable<Pet> AllPetsButNotMice()
+        {
+			return AllOfSpecie(mpet => mpet.species != Species.Mouse);
+		}
+
+		public IEnumerable<Pet> AllMice()
+        {
+            return AllOfSpecie(mpet => mpet.species == Species.Mouse);
+        }
+
+
+		public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits() {
+			return AllOfSpecie(mpet => mpet.species == Species.Rabbit || mpet.yearOfBirth > 2011);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs() {
+	        return AllOfSpecie(mpet => mpet.species == Species.Dog && mpet.sex == Sex.Male);
+        }
+
+		public IEnumerable<Pet> AllDogsBornAfter2010() {
+			return AllOfSpecie(mpet => mpet.species == Species.Dog && mpet.yearOfBirth > 2010);
+                }
+		public IEnumerable<Pet> AllPetsBornAfter2010() {
+			return AllOfSpecie(mpet => mpet.yearOfBirth > 2010);
+                }
+
+		public IEnumerable<Pet> AllFemalePets() {
+			return AllOfSpecie(mpet => mpet.sex == Sex.Female);
+
+		}
+		public IEnumerable<Pet> AllCatsOrDogs()
+		{
+			return AllOfSpecie(mpet => mpet.species == Species.Cat || mpet.species == Species.Dog);
+		}
+		public PetShop(IList<Pet> petsInTheStore)
         {
             this._petsInTheStore = petsInTheStore;
         }
