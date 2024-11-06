c8eb944e5adb4ae78d987173c0f7a2389ca1d71a
evilbc <pawel.jastrzebski01@gmail.com>
2024-11-06 12:46:38 +0100

    implementacja testów

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -41,5 +41,56 @@ namespace Training.DomainClasses
             result.Sort((pet1, pet2) => pet1.name.CompareTo(pet2.name));
             return result;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return AllPetsBy(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return AllPetsBy(pet => pet.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return AllPetsBy(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return AllPetsBy(pet => pet.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return AllPetsBy(pet => pet.yearOfBirth > 2010);
+
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return AllPetsBy(pet => pet.species == Species.Dog && pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return AllPetsBy(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return AllPetsBy(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
+        }
+
+        private IEnumerable<Pet> AllPetsBy(Predicate<Pet> predicate)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (predicate.Invoke(pet))
+                    yield return pet;
+            }
+
+        }
     }
 }
