0fc81e7e1e505043615c9cf5a20a1e64401ad099
Mateusz <mateusz.naklicki06@gmail.com>
2024-11-06 12:48:44 +0100

    Find feature

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -41,5 +41,56 @@ namespace Training.DomainClasses
             result.Sort((pet1, pet2) => pet1.name.CompareTo(pet2.name));
             return result;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+
+            return OnePetThat(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return OnePetThat(pet => pet.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return OnePetThat(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return OnePetThat(pet => pet.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return OnePetThat(pet => pet.yearOfBirth > 2010);
+        }
+
+        private IEnumerable<Pet> OnePetThat(Func<Pet, bool> condition)
+        {
+            foreach (Pet pet in _petsInTheStore)
+            {
+                if (condition(pet))
+                    yield return pet;
+            }
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return OnePetThat(pet => pet.species == Species.Dog && pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return OnePetThat(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+
+            return OnePetThat(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
+        }
     }
 }
