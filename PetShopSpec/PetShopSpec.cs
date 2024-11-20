347bbef9aee0f7ae96c5528ede6a87373b170955
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2024-11-20 13:52:00 +0100

    dynamic condition making

--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using Training.DomainClasses;
 using Machine.Specifications;
+using PetShop;
 using It = Machine.Specifications.It;
 
 namespace Training.Specificaton
 {
-    public abstract class pet_shop_concern : Specification<PetShop>
+    public abstract class pet_shop_concern : Specification<DomainClasses.PetShop>
     {
         Establish context = () =>
         {
@@ -98,7 +99,7 @@ namespace Training.Specificaton
         private static Pet fluffy_the_second;
     }
 
-    [Subject(typeof(PetShop))]
+    [Subject(typeof(DomainClasses.PetShop))]
     class when_trying_to_change_returned_collection_of_pets : pet_shop_concern
     {
         Establish c = () => pet_initial_content.AddManyItems(new Pet { name = "Pixie" }, new Pet { name = "Dixie" });
@@ -205,7 +206,8 @@ namespace Training.Specificaton
     {
         private It should_be_able_to_find_all_cats = () =>
         {
-            var foundPets = subject.AllCats();
+            Criteria<Pet>criteria = Where_Pet.HasAn(pet => pet.species).EqualTo(Species.Cat);
+            var foundPets = subject.AllCats().AllThat(criteria);
             foundPets.ShouldContainOnly(cat_Tom, cat_Jinx);
         };
         private It should_be_able_to_find_all_mice = () =>
