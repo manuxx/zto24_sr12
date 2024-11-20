7bc72e5236673a612d93d90ae80b928d94ca3139
Mateusz <mateusz.naklicki06@gmail.com>
2024-11-20 13:06:40 +0100

    Add conjunction and alternative

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -40,17 +40,18 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
         {
-            return _petsInTheStore.AllThat((pet => pet.species==Species.Dog && pet.yearOfBirth >2010));
+            return _petsInTheStore.AllThat(new Conjunction<Pet>((Pet.IsBornAfter(2010)), Pet.IsASpeciesOf(Species.Dog)));
         }
 
         public IEnumerable<Pet> AllMaleDogs()
         {
-            return _petsInTheStore.AllThat((pet => pet.sex==Sex.Male && pet.species == Species.Dog));
+            return _petsInTheStore.AllThat(new Conjunction<Pet>(new Negation<Pet>(Pet.IsFemale()), Pet.IsASpeciesOf(Species.Dog)));
+            ;
         }
 
         public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
         {
-            return _petsInTheStore.AllThat((pet => pet.yearOfBirth >2011 || pet.species == Species.Rabbit));
+            return _petsInTheStore.AllThat(new Alternative<Pet>(Pet.IsBornAfter(2011), Pet.IsASpeciesOf(Species.Rabbit)));
         }
 
         public IEnumerable<Pet> AllMice()
@@ -77,10 +78,76 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
+<<<<<<< Updated upstream
             return _petsInTheStore.AllThat((pet => pet.species != Species.Mouse));
+=======
+            return _petsInTheStore.AllThat(new Negation<Pet>(Pet.IsASpeciesOf(Species.Mouse)));
+>>>>>>> Stashed changes
 
         }
 
        
+    }
+
+    public class Conjunction<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem>[] _criteria;
+
+        public Conjunction(params Criteria<TItem>[] criteria)
+        {
+            _criteria = criteria;
+        }
+
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            foreach (var criterion in _criteria)
+            {
+                if (!criterion.IsSatisfiedBy(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class Alternative<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem>[] _criteria;
+
+        public Alternative(params Criteria<TItem>[] criteria)
+        {
+            _criteria = criteria;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            foreach (var criterion in _criteria)
+            {
+                if (criterion.IsSatisfiedBy(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+
+    public class Negation<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> _isASpeciesOf;
+
+        public Negation(Criteria<TItem> isASpeciesOf)
+        {
+            _isASpeciesOf = isASpeciesOf;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return !_isASpeciesOf.IsSatisfiedBy(item);
+        }
     }
 }
