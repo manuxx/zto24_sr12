c0c3711adc0ead1784828b0fa29dc20a426bc72a
mikibak <107919074+mikibak@users.noreply.github.com>
2024-11-20 13:48:52 +0100

    Added criteria builder

---
+++ PetShop/CriteriaBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.DomainClasses;
+
+namespace PetShop
+{
+	public class Where_Pet
+	{
+		public static CriteriaBuilder HasAn(Func<Pet, Species> fieldSelector)
+		{
+			return new CriteriaBuilder(fieldSelector);
+		}
+	}
+
+	public class CriteriaBuilder
+	{
+		private readonly Func<Pet, Species> _fieldSelector;
+
+		public CriteriaBuilder(Func<Pet, Species> fieldSelector)
+		{
+			_fieldSelector = fieldSelector;
+		}
+
+		public Criteria<Pet> EqualTo(Species species)
+		{
+			return new AnonymousCriteria<Pet>(pet => _fieldSelector(pet) == species);
+		}
+	}
+}
