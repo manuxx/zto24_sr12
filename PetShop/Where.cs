c3d29128db3fa327b9148534e4688f8c145873be
Pawel Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-11-20 13:48:57 +0100

    Where

---
+++ PetShop/Where.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Training.DomainClasses;
+
+namespace Training
+{
+	public class Where<T>
+	{
+		public static bool HasAn(Func<object, object> value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class CriteriaBuilder
+	{
+		private readonly Func<Pet, Species> _fieldSelector;
+
+		public CriteriaBuilder(Func<Pet, Species> fieldSelector)
+		{
+			_fieldSelector = fieldSelector;
+		}
+
+		public Criteria<Pet> IsEqual(Species species)
+		{
+			return new Pet.SpeciesCriteria(species);
+		}
+	}
+}
